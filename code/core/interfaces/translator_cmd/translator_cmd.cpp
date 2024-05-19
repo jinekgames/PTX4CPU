94eb287b96e22e9e32a2ab1480ad876eaa754f86
Eugene Kalinin <ujinval@yandex.ru>
2024-05-19 12:13:45 +0100

    save output

--- code/core/interfaces/translator_cmd/translator_cmd.cpp
+++ code/core/interfaces/translator_cmd/translator_cmd.cpp
@@ -42,6 +42,39 @@ auto ParseArgsJson(const std::string& json) {
     return std::unique_ptr<PtxExecArgs>{rawPtr};
 }
 
+bool SaveOutputData(const std::string& filepath, PtxExecArgs& execArgs) {
+
+    std::string output;
+    EMULATOR_SerializeArgsJson(execArgs, output);
+    
+    if (output.empty()) {
+        std::cout << "ERROR: Failed to serialize output values" << std::endl;
+        return false;
+    }
+
+    std::ostream* pOut = &std::cout;
+    std::ofstream fout;
+
+    bool ret = true;
+
+    if (filepath.empty()) {
+        std::cout << "Execution output:" << std::endl << std::endl;
+    } else {
+        fout.open(filepath);
+        if(fout.is_open()) {
+            pOut = &fout;
+            std::cout << "Saving execion output to '" << filepath << "'" << std::endl;
+        } else {
+            std::cout << "ERROR: File '" << filepath << "' opening failed. "
+                      << "Execution output will be printed to console:" << std::endl << std::endl;
+            ret = false;
+        }
+    }
+
+    *pOut << output;
+    return ret;
+}
+
 } // anonimous namespace
 
 // @todo implementation: add ability to export parsed ptx file
@@ -58,16 +91,22 @@ See usage docs here:
 
 Commands:
    --help      - show this message
-   --test-load - do a test load and preparsing of a .ptx file
-                 Example:
-                     TranslatorCmd.exe --test-load input_file.ptx
-   --run       - do a test run of a given kernel with empty arguments
+   --run       - runs a given kernel
                  Params:
                      --kernel  - name of kernel to execute
                      --args    - path to the .json contaning execution arguments
                      --threads - count of kernel execution threads
+                     --save-output  - [optional] path to the .json where execution output will be written
+                                      if argument was not specified, output will be printed to console
+                                      if argument was specified with empty value, original arguments .json will be used
                  Example:
-                     TranslatorCmd.exe --test-run input_file.ptx --kernel _Z9addKernelPiPKiS1_
+                     TranslatorCmd.exe --test-run input_file.ptx --kernel _Z9addKernelPiPKiS1_ --args arguments.json --save-output
+   --test-load - do a test load and preparsing of a .ptx file
+                 Example:
+                     TranslatorCmd.exe --test-load input_file.ptx
+   --test-json - tests if an arguments configuration .json is valid
+                 Example:
+                     TranslatorCmd.exe --test-json arguments.json
 )" << ASCII_LOGO_CUDA4CPU << std::endl;
 
         return 0;
@@ -92,6 +131,19 @@ Commands:
         std::cout << "Translator was successfully created" << std::endl;
         return 0;
 
+    } else if (args.Contains("test-json")) {
+
+        std::string argsJsonPath = args["test-json"];
+        
+        auto pExecVars = ParseArgsJson(ReadFile(argsJsonPath));
+        if (!pExecVars) {
+            std::cout << "ERROR: Parsing of the execution arguments json failed" << std::endl;
+            return 1;
+        }
+
+        std::cout << "Json is Valid" << std::endl;
+        return 0;
+
     } else if (args.Contains("test-run")) {
 
         const std::string inputPath = args["test-run"];
@@ -110,6 +162,16 @@ Commands:
             return 1;
         }
         const auto threadsCount = static_cast<uint32_t>(std::stol(args["threads"]));
+        std::string outputJsonPath;
+        if (args.contains("save-output")) {
+            outputJsonPath = args["save-output"];
+            if (outputJsonPath.empty()) {
+                outputJsonPath = argsJsonPath;
+            }
+        }
+
+        std::cout << "Starting execution of kernel '" << kernelName << "'from '"
+                  << inputPath << "' in " << threadsCount << " treads" << std::endl;
 
         // Parse PTX execution arguments
 
@@ -143,18 +205,9 @@ Commands:
         auto res = pTranslator->ExecuteFunc(kernelName, *pExecVars, gridSize);
 
         if (res) {
-            std::string output;
-            EMULATOR_SerializeArgsJson(*pExecVars, output);
-
-            std::cout << "\nOutput:\n\n" << output << std::endl << std::endl;
-
-            if (output.empty()) {
-                std::cout << "ERROR: Failed to serialize output values" << std::endl;
-                return 1;
-            }
-
             std::cout << "Kernel finished execution" << std::endl;
-            return 0;
+            auto res = SaveOutputData(outputJsonPath, *pExecVars);
+            return (res) ? 0 : 1;
         }
 
         std::cout << "Function execution faled. Error: " << res.msg << std::endl;
@@ -163,7 +216,7 @@ Commands:
     }
 
         std::cout <<
-R"(No run command was specified.
+R"(Invalid command was specified.
 
 Run with '--help' to see available commands
 )" << std::endl;
