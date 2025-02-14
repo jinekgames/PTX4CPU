36acf2a1c5b6e443395446e3a5890ab22b198c50
Eugene Kalinin <ujinval@yandex.ru>
2025-02-15 00:23:00 +0300

    fixed version definition

--- code/core/emulator/utils/version.h
+++ code/core/emulator/utils/version.h
@@ -1,4 +1,18 @@
 #pragma once
 
 
-extern "C" const char ProjectGitCommit[41];
+inline constexpr auto kProductVersion =
+#ifdef PTX4CPU_VERSION
+    PTX4CPU_VERSION;
+#else  // PTX4CPU_VERSION
+    "NO_VERSION_INFORMATION";
+#pragma message("ERROR: Failed to get product version");
+#endif  // PTX4CPU_VERSION
+
+inline constexpr auto kProjectGitCommit =
+#ifdef PROJECT_GIT_COMMIT
+    PROJECT_GIT_COMMIT;
+#else  // PROJECT_GIT_COMMIT
+    "NO_COMMIT_INFORMATION";
+#pragma message("ERROR: Failed to get Git repesitory commit");
+#endif  // PROJECT_GIT_COMMIT
